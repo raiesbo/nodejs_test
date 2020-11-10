f90aa67ee50c21f13770dfc92c86655f4e823340
raiesbo <raiesbo8@gmail.com>
2020-11-10 10:56:17 +0100

    json manipulation

--- 1_NotesApp/app.js
+++ 1_NotesApp/app.js
@@ -1,10 +1,65 @@
-const validator = require('validator');
 const getNotes = require('./notes.js');
+const yargs = require('yargs')
 const chalk = require('chalk');
 
-const msg = getNotes();
+// Customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: "string"
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log('Title: ', argv.title);
+        console.log('Body: ', argv.body)
+    }
+})
+
+// Create Remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Removing the note!')
+    }
+})
+
+// Create List command
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: () => {
+        console.log('Listing out all note!')
+    }
+})
+
+// Create Read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('Reading the note!')
+    }
+})
 
 
-//console.log(msg);
+// add, remove, read, list
 
-console.log(chalk.bgGreen("Success!"), chalk.green("Success!"), chalk.green.bold("Success!"))
+//const command = process.argv[2];
+
+// console.log(process.argv);
+// console.log(yargs.argv);
+
+yargs.parse()
